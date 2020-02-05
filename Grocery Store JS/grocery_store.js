fce4a127a4257364bca2a416d578957afc8046d7
stardustCrystal <60677403+stardustCrystal@users.noreply.github.com>
2020-02-04 16:30:16 -0800

    Add files via upload

---
+++ Grocery Store JS/grocery_store.js
@@ -0,0 +1,11 @@
+var shopper = {
+    
+    name : "Crystal Mullins",
+    age : 38,
+    isfemale : true,
+    profile : function(){
+        return this.name + " "+ this.age;
+    }
+    groceryCart : [milk, bread, eggs, butter, bacon]
+    
+};
